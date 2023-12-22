e31975875b024d65fae657b7c512483f1490f95e
navyney <boontharika_kor@cmu.ac.th>
2023-12-23 05:49:57 +0700

    finished

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    cout << "You will get " << grade[rand()%9] << " in this 261102.";
+    return 0;
+}
